5b8565b3985f00df9fc6065460e1ef5f00d3eb01
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-09-29 18:45:50 +0200

    defined datastructures for future handling of model

--- src/fischer_model.py
+++ src/fischer_model.py
@@ -8,9 +8,9 @@ class FischerResult:
     observable: np.ndarray
     times: np.ndarray
     parameters: list
-    q_arr: list
+    q_values: list
     constants: list
-    y0: np.array
+    y0_t0: (np.array, float)
 
     def to_savedict(self):
         '''Used to store results in database'''
@@ -18,9 +18,9 @@ class FischerResult:
             "observable": apply_marks(self.observable),
             "times": apply_marks(self.times),
             "parameters": apply_marks(self.parameters),
-            "q_arr": apply_marks(self.q_arr),
+            "q_values": apply_marks(self.q_values),
             "constants": apply_marks(self.constants),
-            "y0": apply_marks(self.y0)
+            "y0_t0": apply_marks(self.y0_t0)
         }
         return d
 
@@ -29,6 +29,5 @@ class FischerResult:
 class FischerModel(FischerResult):
     '''Class derived from FischerResult used to store a full singular model description.
     Compared to the FischerResult class, we additionally provide information to solve the ODE.'''
-    rhs: function
-    jacobian: function
-    initial_values: (np.array, float)
+    rhs: callable
+    jacobian: callable
