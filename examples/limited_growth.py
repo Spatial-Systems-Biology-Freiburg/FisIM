739a8fdc16e1dd6c6f56941a43e92468f8d4d35f
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-10-18 23:42:49 +0200

    examples: limited growth: started adapting to new interface

--- examples/limited_growth.py
+++ examples/limited_growth.py
@@ -5,26 +5,34 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 
-# Import custom functions for optimization
-from FisInMa.solving import fischer_determinant
-from FisInMa.data_structures import FischerModel, FischerModelParametrized
-from FisInMa.optimization import find_optimal
-from FisInMa.plotting import plot_all_odes
+# Import custom package to solve such fisher models
+from FisInMa import *
 
 
 # System of equation for pool-model and sensitivities
 ###############################
 ### USER DEFINES ODE SYSTEM ###
 ###############################
-def exp_growth(y, t, Q, P, Const):
+def exp_growth(t, y, Q, P, Const):
     (a,) = P
     (Temp,) = Q
     (n_max,) = Const
-    (n, sa) = y
-    return [
-        Temp * a * (n_max - n),
-        Temp *     (n_max - n) - Temp * a * sa
-    ]
+    (n,) = y
+    return [Temp * a * (n_max - n)]
+
+def dedx(t, y, Q, P, Const):
+    (a,) = P
+    (Temp,) = Q
+    (n_max,) = Const
+    (n,) = y
+    return [[- Temp * a]]
+
+def dedp(t, y, Q, P, Const):
+    (a,) = P
+    (Temp,) = Q
+    (n_max,) = Const
+    (n,) = y
+    return [[- Temp * n]]
 
 
 if __name__ == "__main__":
@@ -50,40 +58,44 @@ if __name__ == "__main__":
 
     # Initial values for complete ODE (with S-Terms)
     n0 = 0.25
-    y0 = np.array([n0, 0])
+    y0 = n0
 
     # Construct parameter hyperspace
     n_times = 4
     n_temps = 3
     
     # Values for temperatures (Q-Values)
-    q_values = [
-        np.linspace(temp_low, temp_high, n_temps)
+    inputs = [
+        (temp_low, temp_high, n_temps)
     ]
 
-    # Create a complete Fischer Model
-    fsm = FischerModel(
-        time_interval=(times_low, times_high),
-        n_times=n_times,
-        parameters=parameters,
-        q_values=q_values,
-        constants=constants,
-        y0=y0,
-        ode_func=exp_growth,
-        criterion_func=fischer_determinant,
-        individual_times=False
+    # Create a complete Fisher Model
+    fsm = FisherModel(
+            ode_fun=exp_growth,
+            ode_dfdx=dedx,
+            ode_dfdp=dedp,
+            ode_y0=y0,
+            ode_t0=times_low,
+            times=(times_low, times_high, n_times),
+            inputs=inputs,
+            parameters=parameters,
+            constants=constants,
     )
+
+    fsmp = FisherModelParametrized.init_from(fsm)
+
+    S, C, solutions = get_S_matrix(fsmp)
 
     ####################
     ### OPTIMIZATION ###
     ####################
-    fsr = find_optimal(fsm, "scipy_differential_evolution", discrete=0.5)
-    print(fsr.times)
-    print(fsr.criterion)
-    d = fsr.criterion
-    solutions = fsr.ode_solutions
+    # fsr = find_optimal(fsm, "scipy_differential_evolution", discrete=0.5, workers=1)
+    # print(fsr.times)
+    # print(fsr.criterion)
+    # d = fsr.criterion
+    # solutions = fsr.ode_solutions
 
     ####################
     ##### PLOTTING #####
     ####################
-    plot_all_odes(fsr)
+    # plot_all_odes(fsr)
