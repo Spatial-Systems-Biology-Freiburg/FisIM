c8acd122c3e595b92afa602fa87ca07f245c6400
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-10-30 21:18:21 +0100

    examples: baranyi: clear up naming, save to json

--- examples/Baranyi_model.py
+++ examples/Baranyi_model.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
+import os, sys
+sys.path.append(os.getcwd())
+
+from FisInMa import *
 
 
-def f(x, t, input, params, consts):
-    (x1, x2, ) = x
-    (Temp, ) = input
-    (x_max, b, Temp_min) = params
+def ode_fun(t, x, u, P, ode_args):
+    x1, x2 = x
+    (Temp, ) = u
+    (x_max, b, Temp_min) = P
     # Define the maximum growth rate
     mu_max = b**2 * (Temp - Temp_min)**2
     return [
@@ -13,11 +17,10 @@ def f(x, t, input, params, consts):
         mu_max * x2                                   # f2
     ]
 
-
-def dfdx(x, t, input, params, consts):
-    (x1, x2, ) = x
-    (Temp, ) = input
-    (x_max, b, Temp_min) = params
+def ode_dfdx(t, x, u, P, ode_args):
+    x1, x2 = x
+    (Temp, ) = u
+    (x_max, b, Temp_min) = P
     mu_max = b**2 * (Temp - Temp_min)**2
     return [
         [
@@ -30,11 +33,10 @@ def dfdx(x, t, input, params, consts):
         ]
     ]
 
-
-def dfdp(x, t, input, params, consts):
-    (x1, x2, ) = x
-    (Temp, ) = input
-    (x_max, b, Temp_min) = params
+def ode_dfdp(t, x, u, P, ode_args):
+    x1, x2 = x
+    (Temp, ) = u
+    (x_max, b, Temp_min) = P
     mu_max = b**2 * (Temp - Temp_min)**2
     return [
         [
@@ -50,40 +52,27 @@ def dfdp(x, t, input, params, consts):
     ]
 
 
-def g(x, t, input, params, consts):
-    (x1, x2, ) = x
-    (Temp, ) = input
-    (x_max, b, Temp_min) = params
+def obs_fun(t, x, u, P, ode_args):
+    x1, x2 = x
+    (Temp, ) = u
+    (x_max, b, Temp_min) = P
     return [x1] # y = x1
 
-
-def dgdx(x, t, input, params, consts):
-    (x1, x2, ) = x
-    (Temp, ) = input
-    (x_max, b, Temp_min) = params
+def obs_dgdx(t, x, u, P, ode_args):
+    x1, x2 = x
+    (Temp, ) = u
+    (x_max, b, Temp_min) = P
     return [
         [1, 0] # [dg/dx1, dg/dx2]
     ]
 
-
-
-def dgdp(x, t, input, params, consts):
-    (x1, x2, ) = x
-    (Temp, ) = input
-    (x_max, b, Temp_min) = params
+def obs_dgdp(t, x, u, P, ode_args):
+    x1, x2 = x
+    (Temp, ) = u
+    (x_max, b, Temp_min) = P
     return [
         [0, 0, 0] # [dg/dx_max, dg/db, dg/dTemp_min]
     ]
-
-
-
-def find_optimal(model):
-    return result
-
-
-class model:
-    def find_optimal(self):
-        return find_optimal(self)
 
 
 if __name__ == "__main__":
@@ -92,7 +81,6 @@ if __name__ == "__main__":
 
     # Define parameters
     p = (np.exp(21.1), 0.038, 2) # (x_max, b, T_min)
-    #p = (1, 2, 3)
     
     # Define ode_args
     c = ()
@@ -101,53 +89,43 @@ if __name__ == "__main__":
     x0 = np.array([np.exp(2.36), 1 / (np.exp(2.66)-1)])
 
     # Define bounds and number of sampling points for times, inputs
-    times = (t_low, t_high, n_t, dt)
+    times = (0.0, 1500.0, 4)
 
     # Example: Define fixed time points
-    times = np.linspace(t_low, t_high, n_t) # [1, 2, 3]
+    # times = [1, 2, 3]
 
+    Temp_low = 4.0
+    Temp_high = 8.0
+    n_Temp = 3
     inputs = [
         # Sample over the range I0_low to I0_high with I0_n values and discretization dI0
-        (I0_low, I0_high, I0_n, dI0),
-        # Define fixed values for second input variable
-        np.array([1, 3, 4, 10]),
-        # Define sampling over range with no discretization
-        (I2_low, I2_high, I2_n)
-        # Same as before: 4th argument is optional
-        (I2_low, I2_high, I2_n, None)
+        np.linspace(Temp_low, Temp_high, n_Temp)
     ]
 
-    model = Model(
+    fsm = FisherModel(
         # Required arguments
-        ode_fun=f,
-        ode_dfdx=dfdx,
-        ode_dfdp=dfdp,
+        ode_x0=x0,
+        ode_t0=0.0,
+        ode_fun=ode_fun,
+        ode_dfdx=ode_dfdx,
+        ode_dfdp=ode_dfdp,
         ode_initial=x0,
         times=times,
         inputs=inputs,
         parameters=p,
         ode_args=c,
         # Optional observable arguments
-        obs_fun=g,#=None
-        obs_dfdx=dgdx,#=None
-        obs_dfdp=dgdp,#=None
-        # Optional arguments for sampling
-        identical_times=False,# TODO!
+        obs_fun=obs_fun,#=None
+        obs_dfdx=obs_dgdx,#=None
+        obs_dfdp=obs_dgdp,#=None
     )
 
-    result = model.find_optimal(
-        # Goal: Only specify optional arguments
-        relative_sensitivities=False,
-        criterion=determinant,
-    )
-    # >>> The optimization found the following time-input combinations as optimal:
-    # >>> array([
-    # >>>   [ ..... ],
-    # >>>   [ ..... ],
-    # >>> ])
+    fsr = find_optimal(fsm, relative_sensitivities=True)
 
-    print(result.observable)
-    print(result.times)
-    print(result.inputs)
+    print(fsr.criterion)
+    for sol in fsr.individual_results:
+        print("Times for inputs", sol.inputs)
+        print(" >> ", sol.times)
 
-    result.plot_all_ode_result()
+    plot_all_solutions(fsr, outdir="out")
+    json_dump(fsr, "save.json")
