92b764e9ec7f59d275096eb76ebc44da1d2009c3
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-10-31 19:23:50 +0100

    examples: baranyi: restructure and increase verbosity

--- examples/Baranyi_model.py
+++ examples/Baranyi_model.py
@@ -6,7 +6,7 @@ sys.path.append(os.getcwd())
 from FisInMa import *
 
 
-def ode_fun(t, x, u, p, ode_args):
+def baranyi_roberts_ode(t, x, u, p, ode_args):
     x1, x2 = x
     (Temp, ) = u
     (x_max, b, Temp_min) = p
@@ -15,6 +15,26 @@ def ode_fun(t, x, u, p, ode_args):
     return [
         mu_max * (x2/(x2 + 1)) * (1 - x1/x_max) * x1, # f1
         mu_max * x2                                   # f2
+    ]
+
+def ode_dfdp(t, x, u, p, ode_args):
+    x1, x2 = x
+    (Temp, ) = u
+    (x_max, b, Temp_min) = p
+    mu_max = b**2 * (Temp - Temp_min)**2
+    return [
+        [
+            mu_max * (x2/(x2 + 1)) * (x1/x_max)**2,                               # df1/dx_max
+             2 * b * (Temp - Temp_min)**2 * (x2/(x2 + 1))
+                * (1 - x1/x_max)*x1,    # df1/db
+            -2 * b**2 * (Temp - Temp_min) * (x2/(x2 + 1))
+                * (1 - x1/x_max)*x1     # df1/dTemp_min
+        ],
+        [
+            0,                                                                    # df2/dx_max
+            2 * b * (Temp - Temp_min)**2 * x2,                                         # df2/db
+            2 * b**2 * (Temp - Temp_min) * x2                                          # df2/dTemp_min
+        ] 
     ]
 
 def ode_dfdx(t, x, u, p, ode_args):
@@ -33,47 +53,6 @@ def ode_dfdx(t, x, u, p, ode_args):
         ]
     ]
 
-def ode_dfdp(t, x, u, p, ode_args):
-    x1, x2 = x
-    (Temp, ) = u
-    (x_max, b, Temp_min) = p
-    mu_max = b**2 * (Temp - Temp_min)**2
-    return [
-        [
-            mu_max * (x2/(x2 + 1)) * (x1/x_max)**2,                               # df1/dx_max
-             2 * b * (Temp - Temp_min)**2 * (x2/(x2 + 1)) * (1 - x1/x_max)*x1,    # df1/db
-            -2 * b**2 * (Temp - Temp_min) * (x2/(x2 + 1)) * (1 - x1/x_max)*x1     # df1/dTemp_min
-        ],
-        [
-            0,                                                                    # df2/dx_max
-            2 * b * (Temp - Temp_min)**2 * x2,                                         # df2/db
-            2 * b**2 * (Temp - Temp_min) * x2                                          # df2/dTemp_min
-        ] 
-    ]
-
-
-def obs_fun(t, x, u, p, ode_args):
-    x1, x2 = x
-    (Temp, ) = u
-    (x_max, b, Temp_min) = p
-    return [x1] # y = x1
-
-def obs_dgdx(t, x, u, p, ode_args):
-    x1, x2 = x
-    (Temp, ) = u
-    (x_max, b, Temp_min) = p
-    return [
-        [1, 0] # [dg/dx1, dg/dx2]
-    ]
-
-def obs_dgdp(t, x, u, p, ode_args):
-    x1, x2 = x
-    (Temp, ) = u
-    (x_max, b, Temp_min) = p
-    return [
-        [0, 0, 0] # [dg/dx_max, dg/db, dg/dTemp_min]
-    ]
-
 
 if __name__ == "__main__":
     # Parameters, ode_args and initial conditions were derived from K. Grijspeerdt1 and P. Vanrolleghem (1999) results
@@ -81,9 +60,6 @@ if __name__ == "__main__":
 
     # Define parameters
     p = (np.exp(21.1), 0.038, 2) # (x_max, b, T_min)
-    
-    # Define ode_args
-    c = ()
 
     # Define initial conditions
     x0 = np.array([np.exp(2.36), 1 / (np.exp(2.66)-1)])
@@ -103,29 +79,33 @@ if __name__ == "__main__":
     ]
 
     fsm = FisherModel(
-        # Required arguments
         ode_x0=x0,
         ode_t0=0.0,
-        ode_fun=ode_fun,
+        ode_fun=baranyi_roberts_ode,
         ode_dfdx=ode_dfdx,
         ode_dfdp=ode_dfdp,
         ode_initial=x0,
         times=times,
         inputs=inputs,
-        parameters=p,
-        ode_args=c,
-        # Optional observable arguments
-        obs_fun=obs_fun,#=None
-        obs_dfdx=obs_dgdx,#=None
-        obs_dfdp=obs_dgdp,#=None
+        parameters=p
     )
 
     fsr = find_optimal(fsm, relative_sensitivities=True)
 
+    # Print the result for the criterion (default=determinant)
     print(fsr.criterion)
+    
+    # Print time points which were chosen by optimization routine
     for sol in fsr.individual_results:
         print("Times for inputs", sol.inputs)
         print(" >> ", sol.times)
 
+    # Plot all ODE results with chosen time points
+    # for different data points
     plot_all_solutions(fsr, outdir="out")
+
+    # Dump all information into a json file
     json_dump(fsr, "baranyi.json")
+
+    # Dumps all information to a string
+    output = json_dumps(fsr)
