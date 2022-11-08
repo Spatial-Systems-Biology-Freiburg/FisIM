7f86ed5d98c97de6697c460bd5f627597a40f55a
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-11-08 17:51:47 +0100

    examples: pool_model with x0 as parameter and observable

--- examples/pool_model.py
+++ examples/pool_model.py
@@ -24,28 +24,60 @@ from FisInMa import *
 ### USER DEFINES ODE SYSTEM ###
 ###############################
 def pool_model(t, y, Q, P, Const):
-    (a, b, c) = P
-    (Temp,) = Q
-    (n0, n_max) = Const
-    (n,) = y
+    a, b = P
+    Temp, = Q
+    n0, c, n_max = Const
+    n, = y
     return [(a*Temp + c) * (n - n0*np.exp(-b*Temp*t))*(1-n/n_max)]
 
 def dfdx(t, y, Q, P, Const):
-    (a, b, c) = P
-    (Temp,) = Q
-    (n0, n_max) = Const
-    (n,) = y
+    a, b = P
+    Temp, = Q
+    n0, c, n_max = Const
+    n, = y
     return (a*Temp + c) * (1-n/n_max) + (a*Temp + c) * (n - n0*np.exp(-b*Temp*t))*(-1/n_max)
 
 def dfdp(t, y, Q, P, Const):
-    (a, b, c) = P
-    (Temp,) = Q
-    (n0, n_max) = Const
-    (n,) = y
+    a, b = P
+    Temp, = Q
+    n0, c, n_max = Const
+    n, = y
     return [
         (Temp) * (n - n0*np.exp(-b*Temp*t))*(1-n/n_max),
-        (a*Temp + c) * (Temp*t*n0*np.exp(-b*Temp*t))*(1-n/n_max),
-        (n - n0*np.exp(-b*Temp*t))*(1-n/n_max)
+        (a*Temp + c) * (Temp*t*n0*np.exp(-b*Temp*t))*(1-n/n_max)
+    ]
+
+def dfdx0(t, y, Q, P, Const):
+    a, b = P
+    Temp, = Q
+    n0, c, n_max = Const
+    n, = y
+    return [
+        [(a*Temp + c) * (- np.exp(-b*Temp*t))*(1-n/n_max)]
+    ]
+
+def g(t, y, Q, P, Const):
+    n0, c, n_max = Const
+    n, = y
+    return [np.log((n+n0)/n0)]
+
+def dgdx(t, y, Q, P, Const):
+    n0, c, n_max = Const
+    n, = y
+    return [
+        [1 / (n+n0)]
+    ]
+
+def dgdp(t, y, Q, P, Const):
+    return [
+        [0, 0]
+    ]
+
+def dgdx0(t, y, Q, P, Const):
+    n0, c, n_max = Const
+    n, = y
+    return [
+        [- n /(n0*(n+n0))]
     ]
 
 
@@ -55,35 +87,34 @@ if __name__ == "__main__":
     ###############################
 
     # Define ode_args for the simulation duration
+    c = 1.31
     n0 = 0.25
     n_max = 2e4
-    Const = (n0, n_max)
+    Const = (n0, c, n_max)
 
     # Define initial parameter guesses
     a = 0.065
-    b = 0.01
-    c = 1.31
-
-    P = (a, b, c)
+    b = 1e-3
+    P = (a, b)
 
     # Initial values for complete ODE (with S-Terms)
     t0 = 0.0
     x0 = n0
 
     # Define bounds for sampling
-    temp_low = 4.0
-    temp_high = 21.0
+    temp_low = 2.0
+    temp_high = 20.0
 
     times_low = t0
     times_high = 16.0
     # Discretize explicitly
     # dtimes = [2.0, 4.0, 5.0]
-    dtimes = 1.0
+    dtimes = 0.5
 
     # Construct parameter hyperspace
     n_times = 6
-    n_temps = 3
-    
+    n_temps = 1
+
     # Values for temperatures (Q-Values)
     inputs = [
         np.linspace(temp_low, temp_high, n_temps)
@@ -95,18 +126,31 @@ if __name__ == "__main__":
             ode_fun=pool_model,
             ode_dfdx=dfdx,
             ode_dfdp=dfdp,
+            ode_dfdx0=dfdx0,
             ode_t0=times_low,
             ode_x0=x0,
-            times=(times_low, times_high, n_times, dtimes),
+            times=(times_low, times_high, n_times),
             inputs=inputs,
             parameters=P,
             ode_args=Const,
+            obs_fun=g,
+            obs_dgdx=dgdx,
+            obs_dgdp=dgdp,
+            obs_dgdx0=dgdx0,
     )
 
     ###############################
     ### OPTIMIZATION FUNCTION ? ###
     ###############################
-    fsr = find_optimal(fsm, relative_sensitivities=True)
+    fsr = find_optimal(
+        fsm,
+        optimization_strategy="scipy_differential_evolution",
+        criterion=fisher_determinant,
+        relative_sensitivities=True,
+        maxiter=200,
+        polish=False,
+        workers=-1,
+    )
 
     ###############################
     ##### PLOTTING FUNCTION ? #####
