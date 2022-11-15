af68a471b737e2666ad194588eed04c6deb72a8d
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-11-15 23:57:28 +0100

    examples: damped osci: reworked example and concluded documentation

--- examples/damped_oscillator.py
+++ examples/damped_oscillator.py
@@ -1,125 +1,86 @@
 #!/usr/bin/env python3
-
-#################################
-# THESE LINES ARE ONLY NEEDED   #
-# WHEN FisInMa IS NOT INSTALLED #
-# OTHERWISE REMOVE THEM         #
-#################################
+# The next 2 lines of code are only needed if executing this example 
+# without installing the package
 import os, sys
 sys.path.append(os.getcwd())
-#################################
 
 import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
-import copy
-
-
-# Import custom functions for optimization
 from FisInMa import *
 
-
-# System of equation for pool-model and sensitivities
-###############################
-### USER DEFINES ODE SYSTEM ###
-###############################
+# Defines the right hand side of the ODE of an parametrized damped oscillator
 def damped_osci(t, y, inputs, parameters, ode_args):
-    d, l = parameters
-    T, H = inputs
+    a, b, l = parameters
+    h, = inputs
     A, B = y
-    x_offset, = ode_args
     return [
-        - l*T*A - d*H*(B-x_offset),
+        -(a*h+b)*A - l*B,
         A
     ]
 
+# Derivative of the RHS of the ODE with respect to the components of the ODE
 def damped_osci_dfdx(t, y, inputs, parameters, ode_args):
-    d, l = parameters
-    T, H = inputs
+    a, b, l = parameters
+    h, = inputs
     A, B = y
-    x_offset, = ode_args
     return [
-        [- l*T, - d*H],
+        [-(a*h+b), -l],
         [1, 0]
     ]
 
+# Derivative with respect to the parameters
 def damped_osci_dfdp(t, y, inputs, parameters, ode_args):
-    d, l = parameters
-    T, H = inputs
+    a, b, l = parameters
+    h, = inputs
     A, B = y
-    x_offset, = ode_args
     return [
-        [- H*(B-x_offset), - T*A],
-        [0, 0]
+        [-h*A, -A, -B],
+        [0, 0, 0]
     ]
 
 
-if __name__ == "__main__":
-    ###############################
-    ### USER DEFINES PARAMETERS ###
-    ###############################    
-
+# The main function executes the script and gathers every needed numerical value.
+if __name__ == "__main__": 
     # Define initial parameter guesses
-    d = 5e-0
-    l = 1e-0
-    parameters = (d, l)
+    a = 3.0
+    b = 1.0
+    l = 5.0
+    parameters = (a, b, l)
 
-    # Define optional arguments for the ODE
-    x_offset = 20
-    ode_args = (x_offset,)
-
-    # Initial values for complete ODE (with S-Terms)
+    # Initial values for ODE
     t0 = 0.0
     x0 = [6.0, 20.0]
 
-    # Define bounds for sampling
-    temp_low = 0.8
-    temp_high = 1.2
+    # Numerical values for the input variable h
+    h_low = 0.08
+    h_high = 0.12
+    n_h = 1
 
-    hum_low = 0.8
-    hum_high = 1.2
-
+    # Numerical values for time points
     times_low = t0
     times_high = 10.0
+    n_times = 3
 
-    # Construct parameter hyperspace
-    n_times = 5
-    n_temps = 1
-    n_hums = 1
-
-    # Values for temperatures and humidities
+    # Gather information on inputs in list
     inputs = [
-        np.linspace(temp_low, temp_high, n_temps),
-        np.linspace(hum_low, hum_high, n_hums),
+        np.linspace(h_low, h_high, n_h)
     ]
 
+    # Create a fisher model containing all information of the system.
     fsm = FisherModel(
             ode_fun=damped_osci,
             ode_dfdx=damped_osci_dfdx,
             ode_dfdp=damped_osci_dfdp,
             ode_t0=times_low,
             ode_x0=x0,
-            times=(times_low, times_high, n_times),
+            times={"lb":times_low, "ub":times_high, "n":n_times},
             inputs=inputs,
             parameters=parameters,
-            ode_args=ode_args,
-            covariance=("abs", 2.0),
+            covariance={"rel": 0.1},
     )
 
-    ###############################
-    ### OPTIMIZATION FUNCTION ? ###
-    ###############################
-    fsr = find_optimal(
-        fsm,
-        optimization_strategy="scipy_differential_evolution",
-        criterion=fisher_determinant,
-        maxiter=100,
-        polish=False,
-        workers=1,
-    )
+    # This finds the optimal time points to estimate the parameters
+    fsr = find_optimal(fsm)
 
-    ###############################
-    ##### PLOTTING FUNCTION ? #####
-    ###############################
+    # Automatically plot the results and export as json file
     plot_all_solutions(fsr, outdir="out")
     json_dump(fsr, "damped_osci.json")
