01220dba4d3ae8031287064943bdd68bec940622
Polina Gaindrik <polinagayndrik@gmail.com>
2023-01-27 16:26:08 +0100

    test: delete old function ode_dfdx, correct new parameter values

--- test/setUp/extended_baranyi.py
+++ test/setUp/extended_baranyi.py
@@ -91,41 +91,6 @@ def ode_dfdx(t, x, u, p, ode_args):
     ]
 
 
-def ode_dfdx(t, x, u, p, ode_args):
-    x1, x2, y1, y2 = x
-    (Temp, ) = u
-    (xy_max, b_x, Temp_min_x, alpha_x, b_y, Temp_min_y, alpha_y, ) = p
-    # Define the maximum growth rate
-    mu_max_x = b_x**2 * (Temp - Temp_min_x)**2
-    mu_max_y = b_y**2 * (Temp - Temp_min_y)**2
-    return [
-        [
-            mu_max_x * (x2/(x2 + 1)) * (1 - 2*x1/xy_max) - alpha_x * y1 / xy_max, # df1/dx1
-            mu_max_x * 1/(x2 + 1)**2 * (1 - x1/xy_max)*x1,                        # df1/dx2
-            - alpha_x * x1 / xy_max,                                              # df1/dy1
-            0,                                                                    # df1/dy2
-        ],
-        [
-            0,                                                                    # df2/dx1
-            mu_max_x,                                                             # df2/dx2
-            0,
-            0
-        ],
-        [
-            - alpha_x * y1 / xy_max,                                              # df3/dx1
-            0,
-            mu_max_y * (y2/(y2 + 1)) * (1 - 2*y1/xy_max) - alpha_y * x1 / xy_max, # df3/dy1
-            mu_max_y * 1/(y2 + 1)**2 * (1 - y1/xy_max)*y1,                        # df3/dy2
-        ],
-        [
-            0,                                                                    # df4/dx1
-            0,                                                                    # df4/dx2
-            0,
-            mu_max_y
-        ]
-    ]
-
-
 class ModelExtendedBaranyi:
     def __init__(self, obs=[0], N_x0=2, n_t0=13, n_times=5, n_inputs=7, identical_times=False):
         # Use prime numbers for sampled parameters to 
@@ -135,7 +100,7 @@ class ModelExtendedBaranyi:
         self.times=np.linspace(0.1, 10.0, n_times)
         self.inputs=[np.arange(3, n_inputs + 3)]
         
-        self.parameters = (1e8, 0.2, 1.0, 3.5, 0.2, 1.0, 3.5)
+        self.parameters = (1e8, 0.2, 1.0, 0.2, 1.0)
         self.fsm = FisherModel(
             ode_fun=baranyi_roberts_ode,
             ode_dfdx=ode_dfdx,
