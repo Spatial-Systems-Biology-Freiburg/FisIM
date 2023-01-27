abe8f30e67401dafa90e6d56e92d0d0ac1d729e9
polinagaindrik <polinagayndrik@gmail.com>
2023-01-27 16:47:00 +0200

    documentation: delete plot function that is not used

--- docs/create_plots.py
+++ docs/create_plots.py
@@ -7,9 +7,7 @@ import FisInMa
 
 
 # Generate plots for the discretization
-from source.getting_started import plot_discretization
 from source.documentation import plot_penalty_functions
 
-plot_discretization.plot_default_discretization(outdir=Path(os.path.dirname(plot_discretization.__file__)))
 plot_penalty_functions.plot_discretization_product(outdir=Path(os.path.dirname(plot_penalty_functions.__file__)))
 plot_penalty_functions.plot_discrete_penalty_individual_template(outdir=Path(os.path.dirname(plot_penalty_functions.__file__)))
